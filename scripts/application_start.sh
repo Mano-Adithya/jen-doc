c1721e34868ad179a2b5ea65b5fe6748129bd168
Mano-Adithya <manoadithyap@gmail.com>
2024-08-07 12:54:17 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status
+set -u  # Treat unset variables as an error and exit immediately
+set -o pipefail  # Return the exit status of the last command in the pipe that failed
+
 # Print a message to indicate the start of the script
 echo "Starting the application..."
 
-# Navigate to the application directory
-cd /var/www/myapp
+# For simple-html application
+if ! docker ps | grep -q simple-html-app; then
+  docker start simple-html-app
+fi
 
-# Start the application
-/usr/bin/npm start 
+# For new-html application
+if ! docker ps | grep -q new-html-app; then
+  docker start new-html-app
+fi
 
 # Print a message indicating that the application has started
 echo "Application started successfully."
