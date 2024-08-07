9f6fabba87ef0e9a691b5c264a134e9f570042a4
Mano-Adithya <manoadithyap@gmail.com>
2024-08-07 10:51:09 +0530

    Update script.js

--- simple-html/script.js
+++ simple-html/script.js
@@ -22,7 +22,7 @@ function createSection(title, description, linkText, linkUrl) {
 
 // Determine which page to display based on the URL or other logic
 if (window.location.pathname.includes('index.html')) {
-    createSection('Welcome to HTML1 by Mano', 'This is the first microservice.', 'Go to HTML2 Microservice', 'http://13.201.73.188:8082');
+    createSection('Welcome to HTML1 by Mano', 'This is the first microservice.', 'Go to HTML2 Microservice', 'http://13.233.119.99:8082');
 } else if (window.location.pathname.includes('index2.html')) {
-    createSection('Hello from Another HTML Page!', 'This is the second microservice.', 'Go to HTML1 Microservice', 'http://13.201.73.188:8081');
+    createSection('Hello from Another HTML Page!', 'This is the second microservice.', 'Go to HTML1 Microservice', 'http://13.233.119.99:8081');
 }
